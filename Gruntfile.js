7d2437b330c6bc8f36c770611304cc03bc2fc13c
abestract <ibrahim.iqbal@gmail.com>
2015-05-22 17:32:51 -0700

    Initial commit with my setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,75 @@
 module.exports = function(grunt) {
-  // Define project configuration
-  var project = {
-    paths: {
-      get config() {
-        return this.grunt + 'config/';
-      },
-      dist: 'dist/',
-      doc: 'doc/',
-      grunt: 'grunt/',
-      js: 'js/',
-      sassLoad: __dirname + '/scss',
-      scss: 'scss/',
-      spec: 'spec/',
-      vendor: grunt.file.readJSON('.bowerrc').directory + '/'
-    },
-    files: {
-      get config() {
-        return project.paths.config + '*.js';
-      },
-      grunt: 'Gruntfile.js',
-      js: ['js/foundation/foundation.js', 'js/foundation/*.js'],
-      scss: ['scss/foundation.scss', 'scss/settings.scss']
-    },
-    pkg: grunt.file.readJSON('package.json')
-  };
 
-  // Load Grunt configurations and tasks
-  require('load-grunt-config')(grunt, {
-    configPath: require('path').join(process.cwd(), project.paths.config),
-    data: project,
-    jitGrunt: {
-      staticMappings: {
+  // Project configuration.
+  grunt.initConfig({
+    watch: {
+      coffee: {
+        files: ['coffee-script/*.coffee'],
+        tasks: 'coffee'
+      },
+      jade: {
+        files: '*.jade',
+        tasks: [ 'jade' ]
+      },
+      css: {
+        files: ['scss/*.scss'],
+          tasks: ['sass'],
+          options: {
+              spawn: false
+            }
+      }
+    },
+    coffee: {
+      compile: {
+        files: [{
+          expand: true,
+          cwd: 'coffee-script',
+          src: '**/*.coffee',
+          dest: 'dist/js',
+          ext: '.js'
+        }]
+      }
+    },
+    sass: {
+      options: {
+        loadPath: ['bower_components/foundation/scss']
+      },
+      dist: {
+          options: {
+              sourcemap: 'none',
+              style: 'nested'
+          },
+          files: [{
+            expand: true,
+            cwd: 'scss',
+            src: ['*.scss'],
+            dest: 'dist/css',
+            ext: '.css'
+          }]
+      } 
+    },
+    jade: {
+      compile: {
+        options: {
+            client: false,
+            pretty: true
+        },
+        files: [ {
+          src: "*.jade",
+          dest: "dist/",
+          expand: true,
+          ext: ".html"
+        } ]
       }
     }
   });
+
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  //grunt.loadNpmTasks('grunt-libsass');
+
+  // Default task.
+  grunt.registerTask('default', ['watch', 'coffee', 'sass', 'jade']);
 };
